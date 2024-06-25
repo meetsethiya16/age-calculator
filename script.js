ea9513fffe0770bee0f4c20f2c4e1c7be076604a
meetsethiya16 <meet.sethiya.16@gmail.com>
2024-06-25 07:06:16 +0530

    calculation issue resolved

--- script.js
+++ script.js
@@ -29,20 +29,29 @@ const toggleDateOfBirthSelector = () => {
 
 const updateAge = () => {
   const currentDate = new Date();
-  const dateDiff = currentDate - dateOfBirth;
-  const year = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365));
-  const month = Math.floor((dateDiff / (1000 * 60 * 60 * 24 * 365)) % 12);
-  const day = Math.floor(dateDiff / (1000 * 60 * 60 * 24)) % 30;
-  const hour = Math.floor(dateDiff / (1000 * 60 * 60)) % 24;
-  const minute = Math.floor(dateDiff / (1000 * 60)) % 60;
-  const second = Math.floor(dateDiff / 1000) % 60;
+  if (dateOfBirth) {
+    const dateDiff = currentDate - dateOfBirth;
+    const year = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365.25));
+    const month = Math.floor(
+      (dateDiff % (1000 * 60 * 60 * 24 * 365.25)) /
+        (1000 * 60 * 60 * 24 * 30.44)
+    );
+    const day = Math.floor(
+      (dateDiff % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24)
+    );
+    const hour = Math.floor(
+      (dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minute = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+    const second = Math.floor((dateDiff % (1000 * 60)) / 1000);
 
-  yearEl.innerHTML = makeTwoDigitNumber(year);
-  monthEl.innerHTML = makeTwoDigitNumber(month);
-  dayEl.innerHTML = makeTwoDigitNumber(day);
-  hourEl.innerHTML = makeTwoDigitNumber(hour);
-  minuteEl.innerHTML = makeTwoDigitNumber(minute);
-  secondEl.innerHTML = makeTwoDigitNumber(second);
+    yearEl.innerHTML = makeTwoDigitNumber(year);
+    monthEl.innerHTML = makeTwoDigitNumber(month);
+    dayEl.innerHTML = makeTwoDigitNumber(day);
+    hourEl.innerHTML = makeTwoDigitNumber(hour);
+    minuteEl.innerHTML = makeTwoDigitNumber(minute);
+    secondEl.innerHTML = makeTwoDigitNumber(second);
+  }
 };
 
 const localStorageGetter = () => {
@@ -50,9 +59,9 @@ const localStorageGetter = () => {
   const month = localStorage.getItem("month");
   const date = localStorage.getItem("date");
   if (year && month && date) {
-    dateOfBirth = new Date(year, month, date);
+    dateOfBirth = new Date(year, parseInt(month), parseInt(date));
+    updateAge();
   }
-  updateAge();
 };
 
 const contentToggler = () => {
@@ -68,7 +77,6 @@ const contentToggler = () => {
 
 const setDOBHandler = () => {
   const dateString = dobInputEl.value;
-
   dateOfBirth = dateString ? new Date(dateString) : null;
 
   if (dateOfBirth) {
@@ -81,7 +89,7 @@ const setDOBHandler = () => {
 
 localStorageGetter();
 contentToggler();
-setInterval(() => updateAge(), 1000);
+setInterval(updateAge, 1000);
 
 settingCogEl.addEventListener("click", toggleDateOfBirthSelector);
 dobButtonEl.addEventListener("click", setDOBHandler);
